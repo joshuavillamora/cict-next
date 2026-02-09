a277c96f55eba41ceaa7d48725483c8628d5289c
Wracell <rhazeljay.gumacal@wvsu.edu.ph>
2026-02-09 13:13:25 +0800

    feat: Implement responsiveness to hero section of program

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -18,20 +18,22 @@ export default function ProgramsPage() {
     <main className="min-h-screen bg-white">
 
       {/* Hero Section */}
-      <section className="relative flex flex-col md:flex-row h-148">
-        <div className="relative w-180 md:w-1/2 h-full">
+      <section className="relative flex md:flex-row w-full md:w-full h-103 md:h-148">
+        <div className="absolute md:relative w-full h-100 md:h-full md:w-1/2">
           <Image
             src="/program_assets/cict.png"
             alt="WVSU CICT Building"
             fill
-            className="object-cover"
+            className="object-cover opacity-70 md:opacity-100"
             priority
-            sizes="(max-width: 768px) 100vw, 50vw"
+            sizes="(max-width: 900px) 100vw, 50vw"
           />
+          {/* Overlay: Only visible on Mobile to make text readable */}
+          <div className="absolute inset-0 bg-linear-to-t from-orange-dark/70 via-orange-dark/20 to-transparent md:hidden"></div>
         </div>
-        <div className="w-180 md:w-1/2 bg-orange-light flex flex-col justify-center p-20 text-white">
-          <h1 className="text-5xl font-medium font-major mb-6">Programs and Curriculum</h1>
-          <p className="max-w-md text-base font-light font-minor leading-relaxed tracking-tight">
+        <div className="relative z-10 flex flex-col justify-center w-full h-full px-6 text-center text-white md:w-1/2 md:bg-orange-light md:text-left md:p-20 ">
+          <h1 className="text-5xl font-medium mb-6 drop-shadow-xl md:drop-shadow-none">Programs and Curriculum</h1>
+          <p className="max-w-md text-base font-light leading-relaxed tracking-tight drop-shadow-xl md:drop-shadow-none">
             Explore our undergraduate and graduate degree programs and take the next step toward your goals.
           </p>
         </div>
