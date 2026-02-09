096e31d19016506a72fb3469d32e278df36fc6dd
Wracell <rhazeljay.gumacal@wvsu.edu.ph>
2026-02-09 13:26:49 +0800

    feat: Implement responsiveness to card section of programs

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -39,17 +39,17 @@ export default function ProgramsPage() {
         </div>
       </section>
       {/* Courses Offered Section */}
-      <section className="max-w-6xl mx-auto pt-27 px-6">
-        <h2 className="text-6xl font-medium text-center mb-16 pb-15 font-oswald tracking-tighter">
+      <section className="max-w-6xl mx-auto pt-9 md:pt-27 px-6">
+        <h2 className="text-4xl md:text-6xl font-medium text-center mb-9 pb-9 md:mb-16 md:pb-15 tracking-tighter">
           Courses Offered
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-22 pb-42">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-17 md:gap-y-22 pb-80 md:pb-42">
           {PROGRAMS.map((program: Program) => (
             <Link
               key={program.id}
               href={program.href}
-              className="group relative bg-white border border-gray-100 rounded-lg p-8 pt-12 flex flex-col items-center text-center cursor-pointer shadow-xl hover:shadow-2xl transition-all duration-300 hover:-translate-y-2"
+              className="group relative bg-white border border-gray-100 rounded-lg p-4 pt-8 md:p-8 md:pt-12 flex flex-col items-center text-center cursor-pointer shadow-xl hover:shadow-2xl transition-all duration-300 hover:-translate-y-2"
             >
               {/* Floating Icon Circle */}
               <div className="absolute -top-8 left-1/2 -translate-x-1/2 w-20 h-20 rounded-full bg-white border-4 border-orange-light flex items-center justify-center transition-all duration-300 group-hover:scale-110 group-hover:border-orange-500">
@@ -64,8 +64,8 @@ export default function ProgramsPage() {
                 </div>
               </div>
 
-              <h3 className="text-3xl font-medium font-oswald tracking-tighter pt-10 mb-2">{program.code}</h3>
-              <p className="px-10 text-base font-inter font-normal mb-14 leading-tight tracking-tight h-10">
+              <h3 className="text-3xl font-medium tracking-tighter pt-10 mb-2">{program.code}</h3>
+              <p className="px-10 text-base font-normal mb-14 leading-tight tracking-tight h-10">
                 {program.name}
               </p>
               <div className="text-black group-hover:text-orange-500 transition-colors transform group-hover:translate-x-1">
